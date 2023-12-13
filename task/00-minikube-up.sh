f9eadf6f5b577d5a4f93a2aaa5f1ddc5021455c4
Karlo Hubak <hubakk@gmail.com>
2023-12-13 21:22:44 +0100

    fix

--- task/00-minikube-up.sh
+++ task/00-minikube-up.sh
@@ -5,16 +5,61 @@ set -e
 # variables
 source ./_init_variables.sh
 
-# check minikube
-command -v minikube 2>&1 || {
-    echo "Could not find minikube on machine...";
-    exit 100
+echo "This script was tested on Fedora Linux 39 (RHEL based distribution)!"
+
+# check if virtualization is enabled
+command -v grep -E -c '(vmx|svm)' /proc/cpuinfo || {
+	echo "Virtualization is not enabled on this host. Enable virtualization or contact your fellow DevOps guy!"
+	exit 100
 }
 
-# check kubectl
+# install dependencies and update the system
+echo "Updating the OS and installing dependencies";
+command -v dnf update -y \
+	&& dnf install -y kernel-devel kernel-headers gcc make perl elfutils-libelf-devel wget
+
+# check if virtualbox is installed
+command -v VBoxManage 2>&1 || {
+	echo "Could not find virtualbox on machine... Installing virtualbox!";
+        echo "Adding VirtualBox package repository...";
+	command wget https://www.virtualbox.org/download/oracle_vbox.asc \
+		&& rpm --import oracle_vbox.asc \
+		&& wget http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -O /etc/yum.repos.d/virtualbox.repo \
+		&& dnf install -y VirtualBox-7.0
+	command -v VBoxManage 2>&1 || {
+		echo "VirtualBox could not be installed, please contact your fellow DevOps guy!"
+		exit 101
+	}
+}
+
+# check if minikube is installed
+command -v minikube 2>&1 || {
+	echo "Could not find minikube on machine... Installing minikube!";
+	command curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm \
+		&& sudo rpm -Uvh minikube-latest.x86_64.rpm
+	command -v minikube 2>&1 || {
+		echo "minikube could not be installed, please contact your fellow DevOps guy!"
+		exit 102
+	}
+}
+
+# check if kubectl is installed
 command -v kubectl 2>&1 || {
-    echo "Could not find kubectl on machine...";
-    exit 101
+  echo "Could not find kubectl on machine... Installing kubectl!";
+	command cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+EOF
+	command dnf upgrade -y \
+		&& dnf install -y kubectl 
+	command -v kubectl 2>&1 || {
+		echo "kubectl could not be installed, please contact the admin!"
+		exit 102
+        }
 }
 
 case "$(uname -s)" in
@@ -27,12 +72,9 @@ esac
 
 ( minikube status ) || minikube start --kubernetes-version ${KUBERNETES_VERSION} --nodes 2
 
-
 # enable metrics server
 minikube addons enable metrics-server
 
 MINIKUBE_IP=`minikube ip`
 
-
-echo
 echo "Cool! MiniKube is up on ${MINIKUBE_IP} address."
