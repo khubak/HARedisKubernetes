9cea98834a8dcd3a340930c405f09c0175befda0
Karlo Hubak <hubakk@gmail.com>
2023-12-21 15:08:04 +0100

    minikube start update

--- task/01-redis-up.sh
+++ task/01-redis-up.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 kubectl create ns redis
-kubectl apply -f ./configs/sc.yaml
-kubectl apply -f ./configs/pv.yaml
+kubectl apply -f ./yamls/sc.yaml
+kubectl apply -f ./yamls/pv.yaml
 kubectl apply -n redis -f ./yamls/redis-config.yaml
+kubectl apply -n redis -f ./yamls/redis-statefulset.yaml
+kubectl apply -n redis -f redis-service.yaml
 
 #check replicationhttps://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands#delete
 kubectl -n redis logs redis-0
