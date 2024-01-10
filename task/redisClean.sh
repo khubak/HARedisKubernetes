b7ef8c5e19f248010013ae0c4b6a3f14d78c9619
Karlo Hubak <hubakk@gmail.com>
2024-01-10 15:49:05 +0100

    fix redisClean.sh

--- task/redisClean.sh
+++ task/redisClean.sh
@@ -8,7 +8,7 @@ read -p "This will delete all data in the Redis database. Are you sure? (y/n): "
 # Check the user's response
 if [ "$confirm" == "y" ]; then
     # Flush the Redis database
-    kubectl exec -n redis redis-0 -- /usr/local/bin/redis-cli -a $REDIS_PWD "FLUSHDB"
+    kubectl exec -n redis redis-0 -- /usr/local/bin/redis-cli -a "opstergo1234" "FLUSHDB"
     echo "Redis database flushed successfully."
 else
     echo "Operation aborted."
